3b90171790fd39bfbfd6f474543494761f59407b
tiendzung <tiendung050803@gmail.com>
2023-10-10 13:23:34 +0700

    Initialize the backend

---
+++ backend/src/test/java/com/FashionStore/FashionStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FashionStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
