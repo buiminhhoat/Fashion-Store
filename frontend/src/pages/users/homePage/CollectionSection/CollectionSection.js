4ae9e3a63d57704d5ebfae84b99877a7008d1df0
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-10-12 16:52:11 +0700

    Update Collection Section

--- frontend/src/pages/users/homePage/CollectionSection/CollectionSection.js
+++ frontend/src/pages/users/homePage/CollectionSection/CollectionSection.js
@@ -1,7 +1,96 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import './style.scss';
 
+import star from '../images/star.svg'
+
+const collectionData = [
+  {
+    tab: { id: "tab-1", title: "Áo Polo" },
+    products: [
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "Đã bán 303",
+        stars: 5,
+      },
+      // Thêm sản phẩm khác vào đây
+    ],
+  },
+  {
+    tab: { id: "tab-2", title: "Áo Tshirt" },
+    products: [
+      // Danh sách sản phẩm cho tab-2
+    ],
+  },
+  {
+    tab: { id: "tab-3", title: "Áo Tanktop" },
+    products: [
+      // Danh sách sản phẩm cho tab-3
+    ],
+  },
+  {
+    tab: { id: "tab-4", title: "Áo Khoác Gió" },
+    products: [
+      // Danh sách sản phẩm cho tab-4
+    ],
+  },
+];
+
+function ProductItem({ product }) {
+  return (
+      <div className="col-3 product-item">
+        <div className="product">
+          <div className="product-img">
+            <a href={product.link}>
+              <img className="show-product-image" lazy-src={product.image} loading="lazy" alt={product.title} src={product.image} />
+            </a>
+            <div className="product-sale-info">
+              <div className="discount-container">
+                <div className="discount-content">
+                  <div className="percent">Giảm</div>
+                  <div className="discount-text">63%</div>
+                </div>
+              </div>
+              <div className="bookmark"></div>
+            </div>
+          </div>
+          <div className="product-info-content">
+            <h3 className="product-title">
+              <a href={product.link}>
+                <div className="text-wrap">{product.title}</div>
+              </a>
+            </h3>
+            <p className="product-price">
+              <span className="sale-price">{product.price}</span>
+              <span className="origin-price">{product.originPrice}</span>
+            </p>
+            <div className="sold">
+              <div className="sold-info">
+                <img src={star} alt="icon star" loading="lazy" />
+                <span>{product.stars}</span>
+              </div>
+              <div className="sold-count">
+                <span>{product.soldCount}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}
+
 const CollectionSection = () => {
+  // State để theo dõi tab đang được chọn
+  const [activeTab, setActiveTab] = useState("tab-1");
+
+  // Hàm để chuyển tab
+  const changeTab = (tabId) => {
+    setActiveTab(tabId);
+  };
+
   return (
       <section className="collection">
         <div className="collection-wrap">
@@ -9,56 +98,27 @@ const CollectionSection = () => {
             <p className="col-4">BỘ SƯU TẬP ÁO NAM</p>
             <div className="col-8 nav-wrap">
               <ul className="nav nav-tabs" id="myTab" role="tablist">
-                <li className="nav-item" role="presentation">
-                  <a
-                      className="nav-link active"
-                      id="nav-collection-63e5ee4fa056b1c6920ed254-tab"
-                      data-bs-toggle="tab"
-                      href="#nav-collection-63e5ee4fa056b1c6920ed254"
-                      role="tab"
-                      aria-selected="true"
-                  >
-                    Áo Polo
-                  </a>
-                </li>
-                <li className="nav-item" role="presentation">
-                  <a
-                      className="nav-link"
-                      id="nav-collection-63e5ee4fa056b1c6920ed255-tab"
-                      data-bs-toggle="tab"
-                      href="#nav-collection-63e5ee4fa056b1c6920ed255"
-                      role="tab"
-                      aria-selected="false"
-                  >
-                    Áo Tshirt
-                  </a>
-                </li>
-                <li className="nav-item" role="presentation">
-                  <a
-                      className="nav-link"
-                      id="nav-collection-63e5ee4fa056b1c6920ed256-tab"
-                      data-bs-toggle="tab"
-                      href="#nav-collection-63e5ee4fa056b1c6920ed256"
-                      role="tab"
-                      aria-selected="false"
-                  >
-                    Áo Tanktop
-                  </a>
-                </li>
-                <li className="nav-item" role="presentation">
-                  <a
-                      className="nav-link"
-                      id="nav-collection-63e5ee4fa056b1c6920ed257-tab"
-                      data-bs-toggle="tab"
-                      href="#nav-collection-63e5ee4fa056b1c6920ed257"
-                      role="tab"
-                      aria-selected="false"
-                  >
-                    Áo Khoác Gió
-                  </a>
-                </li>
+                {collectionData.map((data) => (
+                    <li className="nav-item" key={data.tab.id} role="presentation">
+                      <a
+                          className={`nav-link ${activeTab === data.tab.id ? 'active' : ''}`}
+                          id={`${data.tab.id}-tab`}
+                          data-bs-toggle="tab"
+                          role="tab"
+                          aria-selected={activeTab === data.tab.id ? 'true' : 'false'}
+                          onClick={() => changeTab(data.tab.id)}
+                      >
+                        {data.tab.title}
+                      </a>
+                    </li>
+                ))}
               </ul>
             </div>
+          </div>
+          <div className="product-list">
+            {collectionData.find((data) => data.tab.id === activeTab).products.map((product, index) => (
+                <ProductItem key={index} product={product} />
+            ))}
           </div>
         </div>
       </section>
