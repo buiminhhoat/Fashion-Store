d81057a049ba4b9627be911a87de4297f8582537
tiendzung <tiendung050803@gmail.com>
2023-10-24 13:40:49 +0700

    Add Product Size

---
+++ backend/src/main/java/com/FashionStore/models/ProductSize.java
@@ -0,0 +1,56 @@
+package com.FashionStore.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ProductSize")
+public class ProductSize {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SizeID")
+    private Long sizeID;
+
+    @Column(name = "ProductID")
+    private Long productID;
+
+    @Column(name = "SizeName")
+    private String sizeName;
+
+    public ProductSize(Long productID, String sizeName) {
+        this.productID = productID;
+        this.sizeName = sizeName;
+    }
+
+    public Long getSizeID() {
+        return sizeID;
+    }
+
+    public void setSizeID(Long sizeID) {
+        this.sizeID = sizeID;
+    }
+
+    public Long getProductID() {
+        return productID;
+    }
+
+    public void setProductID(Long productID) {
+        this.productID = productID;
+    }
+
+    public String getSizeName() {
+        return sizeName;
+    }
+
+    public void setSizeName(String sizeName) {
+        this.sizeName = sizeName;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductSize{" +
+                "sizeID=" + sizeID +
+                ", productID=" + productID +
+                ", sizeName='" + sizeName + '\'' +
+                '}';
+    }
+}
