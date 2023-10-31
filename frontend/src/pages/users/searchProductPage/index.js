1744dbd1644c42b40119a6acc9e0cad617cfaf7b
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-11-01 01:00:18 +0700

    Update Search Product Page

--- frontend/src/pages/users/searchProductPage/index.js
+++ frontend/src/pages/users/searchProductPage/index.js
@@ -1,10 +1,134 @@
 import React from "react"
 import "./style.scss"
+import CollectionSection from "../homePage/CollectionSection/CollectionSection";
+
+const shirtCollection = [
+  {
+    tab: { id: "tab-1", title: "Áo Polo" },
+    products: [
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "303",
+        stars: 5,
+      },
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "303",
+        stars: 5,
+      },
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "303",
+        stars: 5,
+      },
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "303",
+        stars: 5,
+      },
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "303",
+        stars: 5,
+      },
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "303",
+        stars: 5,
+      },
+      // Thêm sản phẩm khác vào đây
+    ],
+  },
+  {
+    tab: { id: "tab-2", title: "Áo Tshirt" },
+    products: [
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "303",
+        stars: 5,
+      },
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "303",
+        stars: 5,
+      },
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "303",
+        stars: 5,
+      },
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "303",
+        stars: 5,
+      },
+      {
+        title: "Áo Polo Nam 5S, Thấm Hút Tốt, Thiết kế Basic APC23031",
+        image: "https://5sfashion.vn/storage/upload/images/products/NGrEEBn1PMdR8ZIrv8i4Gvg9U57CJFIqoL2CAoXi.jpg",
+        link: "https://5sfashion.vn/san-pham/ao-polo-nam-5s-chat-lieu-thoang-mat-tham-hut-tot-dung-form-thiet-ke-basic-apc23031",
+        price: "149.000đ",
+        originPrice: "399.000đ",
+        soldCount: "303",
+        stars: 5,
+      },
+    ],
+  },
+  {
+    tab: { id: "tab-3", title: "Áo Tanktop" },
+    products: [
+      // Danh sách sản phẩm cho tab-3
+    ],
+  },
+];
+
 const SearchProductPage = () => {
     return (
-        <>
-            <h1> Search </h1>
-        </>
+        <main id="main">
+          <section className="home-content">
+            <CollectionSection collectionData={shirtCollection} />
+          </section>
+
+        </main>
     );
 }
 
