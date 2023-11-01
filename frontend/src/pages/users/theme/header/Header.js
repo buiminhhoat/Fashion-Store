1abc7f008e532e744f8e3d22a7ed3ad96bf4522c
tiendzung <tiendung050803@gmail.com>
2023-11-01 12:07:08 +0700

    Update Search Dialog

--- frontend/src/pages/users/theme/header/Header.js
+++ frontend/src/pages/users/theme/header/Header.js
@@ -172,6 +172,33 @@ const SearchDialog = () => {
 function SearchBar() {
   // Tạo một trạng thái để theo dõi giá trị nhập vào trường tìm kiếm
   const [searchQuery, setSearchQuery] = useState('');
+  const [showSearchDialog, setShowSearchDialog] = useState(false);
+
+  // Hàm xử lý khi người dùng nhấp vào trường tìm kiếm
+  const handleInputFocus = () => {
+    if (searchQuery) {
+      setShowSearchDialog(true);
+    }
+  };
+
+  // Hàm xử lý khi người dùng thay đổi giá trị nhập vào trường tìm kiếm
+  const handleInputChange = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+    setShowSearchDialog(query !== ''); // Hiển thị SearchDialog nếu có văn bản
+  };
+
+  // Hàm xử lý khi người dùng nhấp chuột ra ngoài trường tìm kiếm
+  const handleInputBlur = () => {
+    setShowSearchDialog(false);
+  };
+
+  // Hàm xử lý khi người dùng bấm phím trong trường tìm kiếm
+  const handleInputKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      setShowSearchDialog(false);
+    }
+  };
 
   return (
       <div className="search-box position-relative">
@@ -184,14 +211,17 @@ function SearchBar() {
               className="input-search form-control w-100 h-100 d-flex align-items-center"
               placeholder="Tìm kiếm sản phẩm ..."
               value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
+              onChange={handleInputChange}
+              onFocus={handleInputFocus}
+              onBlur={handleInputBlur}
+              onKeyDown={handleInputKeyDown}
           />
           <Link to={"/search"}>
             <button className="btn btn-search position-absolute d-flex align-items-center justify-content-center" type="submit">
               <img src={search} className="icon-search" alt="icon search" />
             </button>
           </Link>
-          {searchQuery && (
+          {searchQuery && showSearchDialog && (
               <div>
                 <SearchDialog />
               </div>
