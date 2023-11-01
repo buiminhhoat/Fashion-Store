a7df58dfde678aaf15311def63a151f32d7d7689
buiminhhoat <official.buiminhhoat@gmail.com>
2023-11-02 00:01:59 +0700

    Add Cart Repository

---
+++ backend/src/main/java/com/FashionStore/repositories/CartRepository.java
@@ -0,0 +1,11 @@
+package com.FashionStore.repositories;
+
+import com.FashionStore.models.Cart;
+import com.FashionStore.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findCartByCartID(Long cardID);
+}
