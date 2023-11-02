09504e8373db05958ddf9192e30aa9fa799e5911
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-11-03 03:50:19 +0700

    Update Information Box To Product Detail Content

--- frontend/src/pages/users/productDetailPage/ProductDetailContent/ProductDetailContent.js
+++ frontend/src/pages/users/productDetailPage/ProductDetailContent/ProductDetailContent.js
@@ -199,15 +199,15 @@ const InformationBox = () => {
   }
 
   return (
-      <div className="product-information-box">
+      <div className="product-information-box" style={{marginLeft:"30px", width:"100%"}}>
         <div className="product-name-box">{informationProduct.productName}</div>
 
-        <div className="price-box">
+        <div className="price-box" style={{marginTop:"15px"}}>
            <span className="special-price">{informationProduct.productPrice}.000đ</span>
         </div>
 
         <div className="order-action-box">
-          <div className="wrap-product-detail row me-0 ms-0 mt-12">
+          <div style={{marginTop:"100px"}} className="wrap-product-detail row me-0 ms-0 mt-12">
             <div className="col-3 pe-0 ps-0">
               <p className="wrap-product-detail-title">Kích thước</p>
             </div>
@@ -235,7 +235,7 @@ const InformationBox = () => {
             </div>
           </div>
 
-          <div className="wrap-product-detail product-quantity d-flex">
+          <div style={{marginTop:"30px"}} className="wrap-product-detail product-quantity d-flex">
             <div className="col-3 pe-0 ps-0">
               <div className="wrap-product-detail-title">Số lượng</div>
             </div>
@@ -263,6 +263,48 @@ const InformationBox = () => {
             </div>
           </div>
 
+          <div style={{marginTop:"30px"}} className="wrap-product-detail btn-action d-flex">
+            <div className="d-flex w-100">
+              <button id="add-cart" className="btn-add-to-cart ">
+                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_10_3091)">
+                    <path
+                        d="M10.7917 8.45831C9.34187 8.45831 8.16675 9.63344 8.16675 11.0833C8.16675 12.5332 9.34187 13.7083 10.7917 13.7083C12.2416 13.7083 13.4167 12.5332 13.4167 11.0833C13.4167 9.63344 12.2416 8.45831 10.7917 8.45831ZM11.9584 11.375H11.0834V12.25C11.0834 12.411 10.9527 12.5416 10.7917 12.5416C10.6307 12.5416 10.5001 12.411 10.5001 12.25V11.375H9.62508C9.46408 11.375 9.33341 11.2443 9.33341 11.0833C9.33341 10.9223 9.46408 10.7916 9.62508 10.7916H10.5001V9.91665C10.5001 9.75565 10.6307 9.62498 10.7917 9.62498C10.9527 9.62498 11.0834 9.75565 11.0834 9.91665V10.7916H11.9584C12.1194 10.7916 12.2501 10.9223 12.2501 11.0833C12.2501 11.2443 12.1194 11.375 11.9584 11.375Z"
+                        fill="#C81D31"></path>
+                    <path
+                        d="M7.58337 11.0833C7.58337 9.31145 9.01983 7.87499 10.7917 7.87499C10.8903 7.87499 10.9871 7.88111 11.0834 7.88957V4.37499C11.0834 4.0527 10.8223 3.79166 10.5 3.79166H8.45837V3.00941C8.45837 1.61261 7.40692 0.391115 6.01333 0.297781C4.485 0.195115 3.20837 1.40961 3.20837 2.91666V3.79166H1.16671C0.844416 3.79166 0.583374 4.0527 0.583374 4.37499V13.125C0.583374 13.4473 0.844416 13.7083 1.16671 13.7083H8.95042C8.12471 13.1279 7.58337 12.1695 7.58337 11.0833ZM3.79171 2.91666C3.79171 1.79082 4.70754 0.87499 5.83337 0.87499C6.95921 0.87499 7.87504 1.79082 7.87504 2.91666V3.79166H3.79171V2.91666Z"
+                        fill="#C81D31"></path>
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_10_3091">
+                      <rect width="14" height="14" fill="white"></rect>
+                    </clipPath>
+                  </defs>
+                </svg>
+                <div className="text-btn-action">
+                  <span> Thêm vào giỏ hàng </span>
+                </div>
+              </button>
+              <button className="btn-buy-now ">
+                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_10_3091)">
+                    <path
+                        d="M10.7917 8.45831C9.34187 8.45831 8.16675 9.63344 8.16675 11.0833C8.16675 12.5332 9.34187 13.7083 10.7917 13.7083C12.2416 13.7083 13.4167 12.5332 13.4167 11.0833C13.4167 9.63344 12.2416 8.45831 10.7917 8.45831ZM11.9584 11.375H11.0834V12.25C11.0834 12.411 10.9527 12.5416 10.7917 12.5416C10.6307 12.5416 10.5001 12.411 10.5001 12.25V11.375H9.62508C9.46408 11.375 9.33341 11.2443 9.33341 11.0833C9.33341 10.9223 9.46408 10.7916 9.62508 10.7916H10.5001V9.91665C10.5001 9.75565 10.6307 9.62498 10.7917 9.62498C10.9527 9.62498 11.0834 9.75565 11.0834 9.91665V10.7916H11.9584C12.1194 10.7916 12.2501 10.9223 12.2501 11.0833C12.2501 11.2443 12.1194 11.375 11.9584 11.375Z"
+                        fill="#C81D31"></path>
+                    <path
+                        d="M7.58337 11.0833C7.58337 9.31145 9.01983 7.87499 10.7917 7.87499C10.8903 7.87499 10.9871 7.88111 11.0834 7.88957V4.37499C11.0834 4.0527 10.8223 3.79166 10.5 3.79166H8.45837V3.00941C8.45837 1.61261 7.40692 0.391115 6.01333 0.297781C4.485 0.195115 3.20837 1.40961 3.20837 2.91666V3.79166H1.16671C0.844416 3.79166 0.583374 4.0527 0.583374 4.37499V13.125C0.583374 13.4473 0.844416 13.7083 1.16671 13.7083H8.95042C8.12471 13.1279 7.58337 12.1695 7.58337 11.0833ZM3.79171 2.91666C3.79171 1.79082 4.70754 0.87499 5.83337 0.87499C6.95921 0.87499 7.87504 1.79082 7.87504 2.91666V3.79166H3.79171V2.91666Z"
+                        fill="#C81D31"></path>
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_10_3091">
+                      <rect width="14" height="14" fill="white"></rect>
+                    </clipPath>
+                  </defs>
+                </svg>
+                <span>Mua ngay</span>
+              </button>
+            </div>
+          </div>
 
         </div>
       </div>
