56abc2fb198c809c40ac7c3e4921c286e31e0bd6
buiminhhoat <official.buiminhhoat@gmail.com>
2023-11-03 23:51:57 +0700

    Add API Add Product To Cart

--- backend/src/main/java/com/FashionStore/controllers/CartController.java
+++ backend/src/main/java/com/FashionStore/controllers/CartController.java
@@ -3,25 +3,15 @@ package com.FashionStore.controllers;
 import com.FashionStore.models.*;
 import com.FashionStore.repositories.*;
 import com.FashionStore.security.JwtTokenUtil;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+import java.util.Objects;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -39,6 +29,12 @@ public class CartController {
 
     private final ProductQuantityRepository productQuantityRepository;
 
+    private final CartRepository cartRepository;
+
+    private final CartItemRepository cartItemRepository;
+
+    private final UsersRepository usersRepository;
+
     @Value("${upload_image.dir}")
     String UPLOAD_DIR;
 
@@ -46,12 +42,64 @@ public class CartController {
     public CartController(ProductRepository productRepository, ProductImageRepository productImageRepository,
                           ProductCategoryRepository productCategoryRepository,
                           ProductSizeRepository productSizeRepository,
-                          ProductQuantityRepository productQuantityRepository) {
+                          ProductQuantityRepository productQuantityRepository,
+                          CartRepository cartRepository,
+                          CartItemRepository cartItemRepository,
+                          UsersRepository usersRepository) {
         this.productRepository = productRepository;
         this.productImageRepository = productImageRepository;
         this.productCategoryRepository = productCategoryRepository;
         this.productSizeRepository = productSizeRepository;
         this.productQuantityRepository = productQuantityRepository;
+        this.cartRepository = cartRepository;
+        this.cartItemRepository = cartItemRepository;
+        this.usersRepository = usersRepository;
+    }
+
+    @PostMapping("/add-product-to-cart")
+    public ResponseEntity<?> addProductToCart(HttpServletRequest request) {
+        String accessToken = String.valueOf(request.getParameter("accessToken"));
+        accessToken = accessToken.replace("Bearer ", "");
+        if (!jwtTokenUtil.isTokenValid(accessToken)) {
+            ResponseObject responseObject = new ResponseObject("Token không hợp lệ, vui lòng đăng nhập lại");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseObject);
+        }
+
+        String email = jwtTokenUtil.getEmailFromToken(accessToken);
+        List<Users> findByEmail = usersRepository.findUsersByEmail(email);
+
+        Long userID = findByEmail.get(0).getUserID();
+        Long productID = Long.valueOf(request.getParameter("productID"));
+        Long sizeID = Long.valueOf(request.getParameter("sizeID"));
+        Long quantityPurchase = Long.valueOf(request.getParameter("quantityPurchase"));
+
+
+        Cart cart = new Cart(userID);
+
+        if (cartRepository.findCartByUserID(userID) == null) {
+            cartRepository.save(cart);
+        }
+
+        cart = cartRepository.findCartByUserID(userID);
+
+        Long cartID = cart.getCartID();
+        CartItem cartItem = new CartItem(cartID, productID, sizeID, quantityPurchase);
+
+        CartItem oldCartItem = cartItemRepository.findCartItemByCartIDAndProductID(cartID, productID);
+
+        if (oldCartItem != null && Objects.equals(cartItem.getCartID(), oldCartItem.getCartID())
+            && Objects.equals(cartItem.getProductID(), oldCartItem.getProductID())) {
+            if (Objects.equals(cartItem.getSizeID(), oldCartItem.getSizeID())) {
+                Long oldCartItemQuantity = oldCartItem.getQuantityPurchase();
+                cartItemRepository.delete(oldCartItem);
+                cartItem.setQuantityPurchase(oldCartItemQuantity + cartItem.getQuantityPurchase());
+            }
+        }
+
+        cartItemRepository.save(cartItem);
+
+        ResponseObject responseObject = new ResponseObject(HttpStatus.OK.toString(), "Đã thêm sản phẩm vào giỏ", cartItem);
+        return ResponseEntity.status(HttpStatus.OK).body(responseObject);
     }
 }
 
