32a068f1a238ac34b9d1381efbc7c31e3621a7e2
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-11-05 02:55:48 +0700

    Update Routers

--- frontend/src/utils/router.js
+++ frontend/src/utils/router.js
@@ -3,10 +3,10 @@ import AccountManagementPage from "../pages/admin/accountManagementPage";
 export const ROUTERS = {
     USER: {
         HOME: "/",
-        PROFILE: "profile/*",
-        LOGIN: "login",
-        SEARCH: "search",
-        PRODUCT: "product/:productID"
+        PROFILE: "/profile/*",
+        LOGIN: "/login",
+        SEARCH: "/search",
+        PRODUCT: "/product/:productID"
     },
     ADMIN: {
         PRODUCT_MANAGEMENT: "/admin/product-management-page/*",
