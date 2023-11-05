01f4a367ba6fa30900c0644921a90bba308e97f3
buiminhhoat <official.buiminhhoat@gmail.com>
2023-11-05 15:21:38 +0700

    Add API: Search Category

--- backend/src/main/java/com/FashionStore/repositories/CategoryRepository.java
+++ backend/src/main/java/com/FashionStore/repositories/CategoryRepository.java
@@ -9,5 +9,8 @@ import java.util.List;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findCategoriesByCategoryID(Long categoryID);
     List<Category> findCategoriesByCategoryName(String categoryName);
+    List<Category> findCategoriesByCategoryNameContaining(String categoryName);
     List<Category> findCategoriesByParentCategoryID(Long parentCategoryID);
+
+    List<Category> findCategoriesByParentCategoryIDAndCategoryNameContaining(Long parentCategoryID, String categoryName);
 }
