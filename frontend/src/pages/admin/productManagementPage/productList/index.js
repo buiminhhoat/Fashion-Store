80f2506ca7d1705a17e5bd4dd1188f70f2e4c8d3
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-11-06 02:24:30 +0700

    Update Product List Page

--- frontend/src/pages/admin/productManagementPage/productList/index.js
+++ frontend/src/pages/admin/productManagementPage/productList/index.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./style.scss"
+import {toast} from "react-toastify";
 
 const ProductListPage  = () => {
+  const [selectedCategoriesID, setSelectedCategoriesID] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const fetchData = async () => {
+    const apiGetCategory = "http://localhost:9999/api/get-all-categories";
+    try {
+      const response = await fetch(apiGetCategory, {
+        method: 'GET',
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("apiGetCategory");
+        console.log(data);
+
+        setCategories(data);
+      } else {
+        const data = await response.json();
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error("Không kết nối được với database");
+    }
+  }
+
+  useEffect(() => {
+    fetchData().then(r => {});
+  }, []);
+
+
+  const handleCategoryClick = (categoryID) => {
+      if (selectedCategoriesID.includes(categoryID)) {
+        const updatedCategoriesID = selectedCategoriesID.filter((id) => id !== categoryID);
+        setSelectedCategoriesID(updatedCategoriesID);
+      } else {
+        const updatedCategoriesID = [...selectedCategoriesID, categoryID];
+        setSelectedCategoriesID(updatedCategoriesID);
+      }
+  }
+
   return (
       <div id="app">
         <main id="main">
@@ -17,6 +58,31 @@ const ProductListPage  = () => {
               <div data-v-03749d40="" className="product-edit">
                 <section data-v-03749d40="" className="product-edit__section">
                   <div data-v-2250a4e1="" data-v-54a51dd8="" data-v-03749d40="" className="product-detail-panel product-basic-info" >
+
+
+                    {
+                      categories.map((category, index) => (
+                        <div key={index}>
+                          <div onClick={() => handleCategoryClick(category.categoryID)}>
+                            {category.categoryName}
+                          </div>
+
+                          <div style={{marginLeft:"20px"}}>
+                            {
+                              selectedCategoriesID.find((id) => id === category.categoryID) ?
+                                category.subcategories.map((subcategory, index) => (
+                                  <div onClick={() => handleCategoryClick(subcategory.categoryID)}>
+                                    {subcategory.categoryName}
+                                  </div>
+                                ))
+                              : <></>
+                            }
+                          </div>
+                        </div>
+                      ))
+                    }
+
+
                   </div>
                 </section>
               </div>
