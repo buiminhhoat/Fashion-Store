f8ce1ead0edce993df1900d799b733711ec37f72
buiminhhoat <official.buiminhhoat@gmail.com>
2023-11-05 23:03:26 +0700

    Update Product

--- backend/src/main/java/com/FashionStore/repositories/ProductRepository.java
+++ backend/src/main/java/com/FashionStore/repositories/ProductRepository.java
@@ -3,6 +3,8 @@ package com.FashionStore.repositories;
 import com.FashionStore.models.Product;
 import com.FashionStore.models.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
