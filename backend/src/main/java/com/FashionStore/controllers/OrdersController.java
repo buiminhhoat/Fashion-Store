35a365b88567812f937f86be6498a35e89067861
buiminhhoat <official.buiminhhoat@gmail.com>
2023-11-10 10:14:09 +0700

    Add Orders Controller

---
+++ backend/src/main/java/com/FashionStore/controllers/OrdersController.java
@@ -0,0 +1,62 @@
+package com.FashionStore.controllers;
+
+import com.FashionStore.models.*;
+import com.FashionStore.repositories.*;
+import com.FashionStore.security.JwtTokenUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
+public class OrdersController {
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    private final ProductRepository productRepository;
+    private final ProductImageRepository productImageRepository;
+
+    private final ProductCategoryRepository productCategoryRepository;
+
+    private final ProductSizeRepository productSizeRepository;
+
+    private final ProductQuantityRepository productQuantityRepository;
+
+    private final CategoryRepository categoryRepository;
+
+    private final String appRoot = System.getProperty("user.dir") + File.separator;
+
+    @Value("${upload_image.dir}")
+    String UPLOAD_DIR;
+
+    @Autowired
+    public OrdersController(ProductRepository productRepository, ProductImageRepository productImageRepository,
+                            ProductCategoryRepository productCategoryRepository,
+                            ProductSizeRepository productSizeRepository,
+                            ProductQuantityRepository productQuantityRepository,
+                            CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.productImageRepository = productImageRepository;
+        this.productCategoryRepository = productCategoryRepository;
+        this.productSizeRepository = productSizeRepository;
+        this.productQuantityRepository = productQuantityRepository;
+        this.categoryRepository = categoryRepository;
+    }
+}
+
