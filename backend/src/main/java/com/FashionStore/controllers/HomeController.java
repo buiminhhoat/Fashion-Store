c24430e978d5b416a9d81faf448db75f8e74c8cd
buiminhhoat <official.buiminhhoat@gmail.com>
2023-11-18 19:49:56 +0700

    Update Fashion Store

--- backend/src/main/java/com/FashionStore/controllers/HomeController.java
+++ backend/src/main/java/com/FashionStore/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/**")
+    @GetMapping("/admin/**")
     public String home() {
         return "index"; // Trả về tên của trang chủ (index.html)
     }
