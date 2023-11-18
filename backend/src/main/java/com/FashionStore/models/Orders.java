f559804534d473c71db7913fe1405dfa6028123c
buiminhhoat <official.buiminhhoat@gmail.com>
2023-11-19 02:01:43 +0700

    Update Orders & OrderDetails

--- backend/src/main/java/com/FashionStore/models/Orders.java
+++ backend/src/main/java/com/FashionStore/models/Orders.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Table(name = "orders")
+@Table(name = "Orders")
 public class Orders {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
