8a5ed6a0d77e30cd99545e1ab61f76dea35c15f3
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-11-19 18:17:42 +0700

    Update Confirm Dialog

--- frontend/src/components/dialogs/ConfirmDialog/ConfirmDialog.js
+++ frontend/src/components/dialogs/ConfirmDialog/ConfirmDialog.js
@@ -7,14 +7,14 @@ const ConfirmDialog = ({title, subTitle, titleBtnAccept, titleBtnCancel, onAccep
       <div className="modal fade show" id="modal-auth" tabIndex="-1" aria-labelledby="exampleModalLabel"
            style={{ display: 'block', paddingLeft: '0px' }} aria-modal="true" role="dialog">
         <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content" style={{color:"#333333", padding:"30px"}}>
-            <div style={{marginBottom:"23px"}}>
-              <span style={{fontSize:"30px", fontWeight:"900"}}>{title}</span>
+          <div className="modal-content" style={{color:"#333333", padding:"30px", width:"550px"}}>
+            <div style={{marginBottom:"13px"}}>
+              <span style={{fontSize:"23px", fontWeight:"900"}}>{title}</span>
             </div>
-            <div style={{fontSize:"18px", fontWeight:"800", color:"#4f525d", wordWrap: "break-word", lineHeight: "1.8"}}>
+            <div style={{fontSize:"16px", fontWeight:"800", color:"#4f525d", wordWrap: "break-word", lineHeight: "1.8"}}>
               <span>{subTitle}</span>
             </div>
-            <div className="button-container" style={{marginTop:"50px"}}>
+            <div className="button-container" style={{marginTop:"40px"}}>
               <button type="button" className="confirm-dialog-btn" onClick={onAccept}>{titleBtnAccept}</button>
               <button type="button" className="confirm-dialog-btn confirm-dialog-btn-cancel" onClick={onCancel}>{titleBtnCancel}</button>
             </div>
