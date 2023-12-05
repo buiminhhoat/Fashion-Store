4baf04d4882cb375be429d5c94f50923e7922089
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-12-05 23:09:44 +0700

    Add Account List Page

---
+++ frontend/src/pages/admin/managementPage/accountManagement/accountList/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.scss"
+
+const AccountListPage = () => {
+  return (
+      <div id="app">
+        AccountList
+      </div>
+  );
+}
+
+export default AccountListPage;
