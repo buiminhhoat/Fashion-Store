60089b4e9787c4c68f670ed7a264a642bd422756
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-12-07 06:00:15 +0700

    Add Breadcrumb in Add Account Page

--- frontend/src/pages/admin/managementPage/accountManagement/addAccount/index.js
+++ frontend/src/pages/admin/managementPage/accountManagement/addAccount/index.js
@@ -3,8 +3,16 @@ import "./style.scss";
 
 const AddAccountPage = () => {
   return (
-      <div>
-        AddAccountPage
+      <div id="app">
+        <main id="main">
+          <div className="container profile-wrap">
+            <div className="breadcrumb-wrap">
+              <a href="/">Trang chủ</a>
+              &gt; <span>Quản lý người dùng</span>
+              &gt; <span>Thêm người dùng</span>
+            </div>
+          </div>
+        </main>
       </div>
   );
 };
