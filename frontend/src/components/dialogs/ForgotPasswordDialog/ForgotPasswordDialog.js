51b8747b1f407fcf95cb37e4c6c2af94ddc680b3
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2023-12-29 23:45:38 +0700

    Update Forgot Password Dialog

--- frontend/src/components/dialogs/ForgotPasswordDialog/ForgotPasswordDialog.js
+++ frontend/src/components/dialogs/ForgotPasswordDialog/ForgotPasswordDialog.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import "./style.scss"
 import {DIALOGS} from "../utils/const";
 import forgot_password_img from "../images/forgot-password.jpg";
+import {Spin} from "antd";
 
 const ForgotPasswordDialog = ({ onClose, onSwitch }) => {
   const [storeInfo, setStoreInfo] = useState({
@@ -13,6 +14,8 @@ const ForgotPasswordDialog = ({ onClose, onSwitch }) => {
     openingHours: "",
     storeInformationID: "",
   });
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -37,7 +40,11 @@ const ForgotPasswordDialog = ({ onClose, onSwitch }) => {
         console.log(data.data)
         setStoreInfo(data.data);
       }
-    } catch (error) {
+    }
+    catch (error) {
+    }
+    finally {
+      setIsLoading(false);
     }
   }
 
@@ -46,51 +53,62 @@ const ForgotPasswordDialog = ({ onClose, onSwitch }) => {
   }, []);
 
   return (
-      <div className="modal fade show" id="modal-forgot-password" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-modal="true" role="dialog" style={{ display: 'block', paddingLeft: '0px' }}>
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-body">
-              <div className="title-header-wrap" style={{marginTop:"5px"}}>
-                <span className="title">Quên mật khẩu</span>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleButtonCloseClick}></button>
-              </div>
-              <div className="description-wrap" style={{fontSize: "18px"}}>
-                <p>Quý khách vui lòng đến trực tiếp cửa hàng </p>
-                <p>
-                  hoặc liên hệ qua hotline{' '}
-                  <span style={{color: "#BD0000", fontWeight: "600"}}>{storeInfo.hotline}</span> để được hỗ trợ.
-                </p>
+      <>
+        { !isLoading ?
+          <div className="modal fade show"
+               id="modal-forgot-password"
+               tabIndex="-1" aria-labelledby="exampleModalLabel"
+               aria-modal="true" role="dialog"
+               style={{ display: 'block', paddingLeft: '0px' }}
+          >
+            <div className="modal-dialog modal-dialog-centered">
+              <div className="modal-content">
+                <div className="modal-body">
+                  <div className="title-header-wrap" style={{marginTop:"5px"}}>
+                    <span className="title">Quên mật khẩu</span>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleButtonCloseClick}></button>
+                  </div>
+                  <div className="description-wrap" style={{fontSize: "18px"}}>
+                    <p>Quý khách vui lòng đến trực tiếp cửa hàng </p>
+                    <p>
+                      hoặc liên hệ qua hotline{' '}
+                      <span style={{color: "#BD0000", fontWeight: "600"}}>{storeInfo.hotline}</span> để được hỗ trợ.
+                    </p>
 
-                <img style={{width:"400px", margin:"10px 0 0 25px"}}
-                     src={forgot_password_img}
-                     alt=""
-                />
-              </div>
+                    <img style={{width:"400px", margin:"10px 0 0 25px"}}
+                         src={forgot_password_img}
+                         alt=""
+                    />
+                  </div>
 
-              {/*<div className="form-wrap">*/}
-              {/*  <form onSubmit={handleSubmit} className="form" id="forgot-password">*/}
-              {/*    <div className="input-wrap">*/}
-              {/*      <label className="title">Nhập Email đăng nhập</label>*/}
-              {/*      <input id="email-forgot" name="email" type="email" placeholder="Nhập email" required />*/}
-              {/*      <span className="text-danger error-text forgot-error"></span>*/}
-              {/*    </div>*/}
-              {/*    <div className="btn-wrap">*/}
-              {/*      <button type="submit" className="btn btn-primary btn-login">*/}
-              {/*        <i id="loading-send" className="fa-solid fa-spinner icon-loading"></i>Gửi*/}
-              {/*      </button>*/}
-              {/*    </div>*/}
-              {/*  </form>*/}
-              {/*</div>*/}
-              <div className="register-wrap" style={{margin:"0 0 20px 0"}}>
+                  {/*<div className="form-wrap">*/}
+                  {/*  <form onSubmit={handleSubmit} className="form" id="forgot-password">*/}
+                  {/*    <div className="input-wrap">*/}
+                  {/*      <label className="title">Nhập Email đăng nhập</label>*/}
+                  {/*      <input id="email-forgot" name="email" type="email" placeholder="Nhập email" required />*/}
+                  {/*      <span className="text-danger error-text forgot-error"></span>*/}
+                  {/*    </div>*/}
+                  {/*    <div className="btn-wrap">*/}
+                  {/*      <button type="submit" className="btn btn-primary btn-login">*/}
+                  {/*        <i id="loading-send" className="fa-solid fa-spinner icon-loading"></i>Gửi*/}
+                  {/*      </button>*/}
+                  {/*    </div>*/}
+                  {/*  </form>*/}
+                  {/*</div>*/}
+                  <div className="register-wrap" style={{margin:"0 0 20px 0"}}>
                 <span className="title">
                   Trở lại trang
                   <span className="btn-open-modal-login" onClick={() => handleSwitchToOtherDialog(DIALOGS.LOGIN)}> Đăng nhập</span>
                 </span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+          :
+          <Spin size="large"/>
+        }
+      </>
   );
 }
 
