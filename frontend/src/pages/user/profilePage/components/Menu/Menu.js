2db19abd86aa39f9b41f8bd3ffd75d606124d78f
buiminhhoat <official.buiminhhoat@gmail.com>
2023-12-29 19:29:06 +0700

    Update Menu

--- frontend/src/pages/user/profilePage/components/Menu/Menu.js
+++ frontend/src/pages/user/profilePage/components/Menu/Menu.js
@@ -153,15 +153,13 @@ const Menu = () => {
     }
   };
 
-  console.log(userData.avatarPath);
-
   return (
       <div className="col-4 menu-wrap item-row">
         <div className="header-wrap">
           <div className="image-wrap">
             <img style={{width:"64px", height:"64px"}}
                 src={(userData.avatarPath !== undefined && userData.avatarPath !== null) ?
-                    + userData.avatarPath :
+                    userData.avatarPath :
                     "https://t4.ftcdn.net/jpg/05/49/98/39/240_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg"}
                 alt={''}
                 id="action-upload"
