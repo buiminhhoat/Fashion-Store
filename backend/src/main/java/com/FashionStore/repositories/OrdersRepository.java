d65a623011ebd6030a6a5a6b5c036f7423807cbb
buiminhhoat <official.buiminhhoat@gmail.com>
2023-12-30 15:57:04 +0700

    Update OrdersController

--- backend/src/main/java/com/FashionStore/repositories/OrdersRepository.java
+++ backend/src/main/java/com/FashionStore/repositories/OrdersRepository.java
@@ -4,6 +4,7 @@ import com.FashionStore.models.Orders;
 import com.FashionStore.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
@@ -13,4 +14,7 @@ public interface OrdersRepository extends JpaRepository<Orders, Long> {
     List<Orders> findOrdersByUserIDAndOrderStatus(Long userID, String orderStatus);
 
     List<Orders> findOrdersByRecipientPhone(String recipientPhone);
+
+    List<Orders> findOrdersByOrderDateGreaterThanEqualAndOrderDateLessThanEqual(Date startDate, Date endDate);
+    List<Orders> findOrdersByOrderDate(Date orderDate);
 }
