2519c3e074a9d17926ef4ea03703bd4a47cda4fc
tiendzung <tiendung050803@gmail.com>
2024-01-01 23:11:42 +0700

    Modify logout

--- frontend/src/components/dialogs/utils/logout.js
+++ frontend/src/components/dialogs/utils/logout.js
@@ -1,29 +1,26 @@
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
-// import { useHistory } from "react-router-dom";
 
 export function useLogout() {
     const navigate = useNavigate();
-    const [, , removeAccessTokenCookie] = useCookies(['access_token']);
-    const [, , removeRefreshTokenCookie] = useCookies(['refresh_token']);
+    const [cookies, , removeCookie] = useCookies();
 
     const logout = async () => {
-        // Xóa cookies
-        await Promise.all([
-            removeAccessTokenCookie('access_token'),
-            removeRefreshTokenCookie('refresh_token')
-        ]);
-
-        // Xóa cache
-        await caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    return cacheName.startsWith('your-cache-prefix-');
-                }).map(function(cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
+        // Xóa tất cả các cookies
+        await Object.keys(cookies).forEach(cookieName => {
+            removeCookie(cookieName);
         });
+
+        // Xóa toàn bộ caches
+        if ('caches' in window) {
+            await caches.keys().then(async function(cacheNames) {
+                await Promise.all(
+                    cacheNames.map(async function(cacheName) {
+                        return await caches.delete(cacheName);
+                    })
+                );
+            });
+        }
 
         // Redirect về trang chính của bạn (localhost:3000)
         navigate('/');
