7cecb48034542260779c651ee92a1e23357516bd
buiminhhoat <official.buiminhhoat@gmail.com>
2024-01-02 00:53:15 +0700

    Update logout

--- frontend/src/components/dialogs/utils/logout.js
+++ frontend/src/components/dialogs/utils/logout.js
@@ -1,37 +1,16 @@
-import { useCookies } from "react-cookie";
-import { useNavigate } from "react-router-dom";
+import {useCookies} from "react-cookie";
+import {useNavigate} from "react-router-dom";
 
 export function useLogout() {
     const navigate = useNavigate();
     const [cookies, , removeCookie] = useCookies();
 
-    // const logout = async () => {
-    //     // Xóa tất cả các cookies
-    //     await Object.keys(cookies).forEach(cookieName => {
-    //         removeCookie(cookieName);
-    //     });
-    //
-    //     // Xóa toàn bộ caches
-    //     if ('caches' in window) {
-    //         await caches.keys().then(async function(cacheNames) {
-    //             await Promise.all(
-    //                 cacheNames.map(async function(cacheName) {
-    //                     return await caches.delete(cacheName);
-    //                 })
-    //             );
-    //         });
-    //     }
-    //
-    //     // Redirect về trang chính của bạn (localhost:3000)
-    //     navigate('/');
-    // };
-
-    const logout = async () => {
+    return async () => {
         try {
             // Xóa tất cả các cookies
             const cookieNames = Object.keys(cookies);
             for (const cookieName of cookieNames) {
-                await removeCookie(cookieName, { path: '/' });
+                await removeCookie(cookieName, {path: '/'});
             }
 
             // Xóa toàn bộ caches
@@ -50,6 +29,4 @@ export function useLogout() {
             console.error('Error during logout:', error);
         }
     };
-
-    return logout;
 }
