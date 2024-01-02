70e6a8ebc40051cfec982b02780dde2937c126f6
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2024-01-02 22:09:31 +0700

    Delete Login Page

--- frontend/src/utils/router.js
+++ frontend/src/utils/router.js
@@ -2,7 +2,6 @@ export const ROUTERS = {
     USER: {
         HOME: "/",
         PROFILE: "/profile/*",
-        LOGIN: "/login",
         SEARCH: "/search/:keyword",
         CHECKOUT: "/checkout",
         PRODUCT: "/product",
