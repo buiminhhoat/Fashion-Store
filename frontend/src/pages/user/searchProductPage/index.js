d8c85f7754244640aeca074943bcb0ce4aa749c0
Nguyen Chau Khanh <chaukhanh0605@gmail.com>
2024-01-03 00:29:39 +0700

    Update Search Product Page

--- frontend/src/pages/user/searchProductPage/index.js
+++ frontend/src/pages/user/searchProductPage/index.js
@@ -11,6 +11,8 @@ import fillterIcon from "../categoryPage/images/bars-filter.svg";
 
 import {ScrollToTop} from "../../../utils";
 import {API, FILTERS, MESSAGE, NUMBER_PRODUCT_LIMIT, SEARCH_PRODUCT_PAGE, SORT} from "../../../utils/const";
+import {ConfigProvider, Select} from "antd";
+import {SEARCH_USER} from "../../admin/managementPage/productManagement/utils/const";
 
 const SearchProductPage = () => {
   // Sử dụng useLocation để lấy đường dẫn URL hiện tại
@@ -56,11 +58,6 @@ const SearchProductPage = () => {
   const filteredProductsData = productsData;
   const hasResult = filteredProductsData.length > 0;
 
-  const handleSelectChange = (event) => {
-    const selectedValue = event.target.value;
-    setSelectedSort(selectedValue);
-  };
-
   return (
       <main id="main">
         <ScrollToTop />
@@ -78,17 +75,31 @@ const SearchProductPage = () => {
                     <div className="other-item d-flex align-items-center">
                       <div className="sort-box d-flex align-items-center">
                         <span className="title-child">{FILTERS.SORT_BY}</span>
-                        <select className="form-select sort-item" onChange={handleSelectChange}>
-                          <option value="">
-                            {FILTERS.SELECT_FILTER_CONDITION}
-                          </option>
-                          <option value={SORT.ASC}>
-                            {FILTERS.PRICE_LOW_TO_HIGH}
-                          </option>
-                          <option value={SORT.DECS} >
-                            {FILTERS.PRICE_HIGH_TO_LOW}
-                          </option>
-                        </select>
+
+                        <ConfigProvider
+                            theme={{
+                              components: {
+                                Select: {
+                                  controlItemBgActive: '#ffe6e6',
+                                },
+                              },
+                            }}
+                        >
+                          <Select
+                              defaultValue={""}
+                              style={{ width: 170 }}
+                              bordered={false}
+                              size={"small"}
+                              options={[
+                                { value: "", label: FILTERS.SELECT_FILTER_CONDITION },
+                                { value: SORT.ASC, label: FILTERS.PRICE_LOW_TO_HIGH },
+                                { value: SORT.DECS, label: FILTERS.PRICE_HIGH_TO_LOW },
+                              ]}
+                              onChange={(value) => setSelectedSort(value)}
+                          />
+                        </ConfigProvider>
+
+
                       </div>
                     </div>
                   </div>
